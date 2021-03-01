70b6daf60034dc7db4dd25c7e6eb4b9993fc7ff8
Linus Karlsson <linka107@student.liu.se>
2021-03-01 17:12:48 +0100

    Intuitiv Join/Host

---
+++ Assets/ConnectUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ConnectUI : MonoBehaviour
+{
+    [SerializeField] private GameObject UI;
+    public GameObject networkManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkManager = GameObject.Find("NetworkManager");
+
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            //UI.SetActive(true);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Refresh the list of joinable servers
+    public void RefreshList()
+    {
+        networkManager.GetComponent<Mirror.Discovery.NetworkDiscoveryHUD_Custom>().RefreshList();
+    }
+}
