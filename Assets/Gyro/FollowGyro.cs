07e29de5571d38f626e27cf80a946b43517e4b25
Max Benecke <maxbe682@student.liu.se>
2021-03-22 14:55:03 +0100

    Galenskap fungerar förmodligen ej

--- Assets/Gyro/FollowGyro.cs
+++ Assets/Gyro/FollowGyro.cs
@@ -7,14 +7,38 @@ public class FollowGyro : MonoBehaviour
     [Header("Tweaks")]
     [SerializeField] private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
     private Vector3 initial = new Vector3(90, 180, 0);
+    private Transform appliedGyroRotation;
+    private float appliedY = 0;
+    private float initialY = 0;
+    private float calibrateY = 0;
 
-    private void Start() {
+    private IEnumerator Start() {
         GyroManager.Instance.EnableGyro();
+        initialY = transform.eulerAngles.y;
+        appliedGyroRotation = new GameObject("Player").transform;
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine(Tja());
     }
+
+    private IEnumerator Tja()
+    {
+        calibrateY = appliedY - initialY;
+        yield return null;
+    }
+ 
 
     private void Update() {
         transform.eulerAngles = initial;
-        transform.localRotation = GyroToUnity(GyroManager.Instance.GetGyroRotation()) * baseRotation;
+        appliedGyroRotation.rotation = Input.gyro.attitude;
+        appliedY = appliedGyroRotation.eulerAngles.y;
+
+        appliedGyroRotation.Rotate(0f, -calibrateY, 0);
+
+
+
+        transform.localRotation = appliedGyroRotation.rotation;
     }
 
     private Quaternion GyroToUnity(Quaternion q)
