38c844b9fd09c755638f11785e3f705d1108827a
Linus Karlsson <linka107@student.liu.se>
2021-03-22 09:47:28 +0100

    Imported Gyro Scripts

---
+++ Assets/Gyro/AccelText.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccelText : MonoBehaviour
+{
+
+    Vector3 accel;
+    void Update()
+    {
+        accel = GameObject.Find("Cube").GetComponent<FollowAccel>().GetAccel();
+        GetComponent<Text>().text = accel.ToString();
+    }
+}
---
+++ Assets/Gyro/FollowAccel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FollowAccel : MonoBehaviour
+{
+
+    Vector3 accel;
+    float magnitude;
+    private void Start()
+    {
+        magnitude = 0.0f;
+    }
+    private void Update()
+    {
+        magnitude = GameObject.Find("accelSlider").GetComponent<Slider>().value;
+        accel = Input.gyro.userAcceleration;
+        GetComponent<Rigidbody>().AddForce(magnitude * accel);
+    }
+
+    public Vector3 GetAccel() {
+
+        return accel;
+    
+    }
+
+}
