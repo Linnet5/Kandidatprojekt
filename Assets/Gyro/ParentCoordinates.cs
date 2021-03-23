c4a8878717ae2354c135768b6c5e7433cc070d3e
Linus Karlsson <linka107@student.liu.se>
2021-03-23 11:31:07 +0100

    vafan

--- Assets/Gyro/ParentCoordinates.cs
+++ Assets/Gyro/ParentCoordinates.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class ParentCoordinates : MonoBehaviour
 {
     GyroManager gm;
+    private float initialYAngle = 0f;
     void Start()
     {
         StartCoroutine("Calibrate");
+        initialYAngle = transform.rotation.y;
     }
 
     IEnumerator Calibrate()
     {
         yield return new WaitForSeconds(2.0f);
         gm = GameObject.Find("Spawned GyroManager").GetComponent<GyroManager>();
-        transform.Rotate(0.0f, gm.GetGyroRotation().eulerAngles.y, 0.0f, Space.World);
+        transform.Rotate(0f, initialYAngle -gm.GetGyroRotation().eulerAngles.y, 0f, Space.World);
     }
 }
