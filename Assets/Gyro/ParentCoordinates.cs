d5a0e4bfe45cf62dc27e9f39a91ae5f49048125d
Linus Karlsson <linka107@student.liu.se>
2021-04-08 15:23:34 +0200

    Tydligare instruktioner samt enklare rotationer

--- Assets/Gyro/ParentCoordinates.cs
+++ Assets/Gyro/ParentCoordinates.cs
@@ -7,9 +7,23 @@ public class ParentCoordinates : MonoBehaviour
     GyroManager gm;
     private float initialYAngle = 0f;
     private GameObject target;
+    private GameObject rotateIcon;
+
+    //Audio variables
+    public AudioSource audioSource;
+    public AudioClip three;
+    public AudioClip two;
+    public AudioClip one;
+    public AudioClip go;
+
     [SerializeField] private GameObject bodyText;
     void Start()
     {
+        rotateIcon = GameObject.Find("counter-clockwise-icon");
+
+        if(rotateIcon.activeSelf)
+            rotateIcon.SetActive(false);
+
         StartCoroutine("Calibrate");
         initialYAngle = transform.rotation.y;
         target = GameObject.Find("Target");
@@ -20,8 +34,26 @@ public class ParentCoordinates : MonoBehaviour
         yield return new WaitForSeconds(3.0f);
         gm = GameObject.Find("Spawned GyroManager").GetComponent<GyroManager>();
         transform.LookAt(new Vector3(target.transform.position.x, 0f, target.transform.position.z));
-        gameObject.GetComponent<Movement>().canMove = true;
         bodyText.GetComponent<TMPro.TextMeshProUGUI>().SetText(
-            "CURRENTLY IN A TRAINING SESSION. REMEMBER TO HAVE THE PHONE IN THE CORRECT POSITION AND FOLLOW THE INSTRUCTIONS.");
+            "Turn your phone counter-clockwise, put it against your chest and lay down. A countdown will indicate when exercise is about to start.");
+        rotateIcon.SetActive(true);
+        StartCoroutine("StartExercise");
+    }
+
+    IEnumerator StartExercise()
+    {
+        yield return new WaitForSeconds(5.0f);
+        audioSource.PlayOneShot(three);
+        yield return new WaitForSeconds(1.0f);
+        audioSource.PlayOneShot(two);
+        yield return new WaitForSeconds(1.0f);
+        audioSource.PlayOneShot(one);
+        yield return new WaitForSeconds(1.0f);
+        audioSource.PlayOneShot(go);
+
+        bodyText.GetComponent<TMPro.TextMeshProUGUI>().SetText(
+        "CURRENTLY IN A TRAINING SESSION. REMEMBER TO HAVE THE PHONE IN THE CORRECT POSITION AND FOLLOW THE INSTRUCTIONS.");
+        rotateIcon.SetActive(false);
+        gameObject.GetComponent<Movement>().canMove = true;
     }
 }
