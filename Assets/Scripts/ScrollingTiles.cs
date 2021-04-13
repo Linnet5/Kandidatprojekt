cfd806edc7127f21a30b589f494e695aff823c16
Linus Karlsson <linka107@student.liu.se>
2021-04-13 11:18:51 +0200

    ForestLevel Setup

---
+++ Assets/Scripts/ScrollingTiles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScrollingTiles : MonoBehaviour
+{
+
+    RawImage sprite;
+
+    [Range(0.0f, 10.0f)]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = gameObject.GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.uvRect = new Rect(sprite.uvRect.x * speed* Time.deltaTime, 
+            sprite.uvRect.y * speed * Time.deltaTime, 
+            sprite.uvRect.width, 
+            sprite.uvRect.height); ; ;
+    }
+}
