ec3d45502fae9da34d9b22334ed1affe75454138
Elidj214 <elidj214@student.liu.se>
2021-04-16 16:37:07 +0200

    Update Elin_Analyze.m

--- Matlab/Elin_Analyze.m
+++ Matlab/Elin_Analyze.m
@@ -161,51 +161,49 @@ figure;
 [Epar1_99, EgyroX_9, Ekoma1_99, EgyroY_9, Ekoma2_99, EgyroZ_9, Epar2_99] = textread('Elin-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
 [Epar1_100, EgyroX_10, Ekoma1_100, EgyroY_10, Ekoma2_100, EgyroZ_10, Epar2_100] = textread('Elin-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
 
-[par1_11, gyroX_1, koma1_11, gyroY_1, koma2_11, gyroZ_1, par2_11] = textread('-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
-[par1_22, gyroX_2, koma1_22, gyroY_2, koma2_22, gyroZ_2, par2_22] = textread('-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
-[par1_33, gyroX_3, koma1_33, gyroY_3, koma2_33, gyroZ_3, par2_33] = textread('-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
-[par1_44, gyroX_4, koma1_44, gyroY_4, koma2_44, gyroZ_4, par2_44] = textread('-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
-[par1_55, gyroX_5, koma1_55, gyroY_5, koma2_55, gyroZ_5, par2_55] = textread('-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
-[par1_66, gyroX_6, koma1_66, gyroY_6, koma2_66, gyroZ_6, par2_66] = textread('-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
-[Epar1_77, EgyroX_7, Ekoma1_77, EgyroY_7, Ekoma2_77, EgyroZ_7, Epar2_77] = textread('Elin-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
-[Epar1_88, EgyroX_8, Ekoma1_88, EgyroY_8, Ekoma2_88, EgyroZ_8, Epar2_88] = textread('Elin-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
-[Epar1_99, EgyroX_9, Ekoma1_99, EgyroY_9, Ekoma2_99, EgyroZ_9, Epar2_99] = textread('Elin-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
-[Epar1_100, EgyroX_10, Ekoma1_100, EgyroY_10, Ekoma2_100, EgyroZ_10, Epar2_100] = textread('Elin-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
+[Lpar1_11, LgyroX_1, Lkoma1_11, LgyroY_1, Lkoma2_11, LgyroZ_1, Lpar2_11] = textread('Linus-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
+[Lpar1_22, LgyroX_2, Lkoma1_22, LgyroY_2, Lkoma2_22, LgyroZ_2, Lpar2_22] = textread('Linus-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
+[Lpar1_33, LgyroX_3, Lkoma1_33, LgyroY_3, Lkoma2_33, LgyroZ_3, Lpar2_33] = textread('Linus-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
+[Lpar1_44, LgyroX_4, Lkoma1_44, LgyroY_4, Lkoma2_44, LgyroZ_4, Lpar2_44] = textread('Linus-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
+[Lpar1_55, LgyroX_5, Lkoma1_55, LgyroY_5, Lkoma2_55, LgyroZ_5, Lpar2_55] = textread('Linus-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
+[Lpar1_66, LgyroX_6, Lkoma1_66, LgyroY_6, Lkoma2_66, LgyroZ_6, Lpar2_66] = textread('Linus-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
+[Lpar1_77, LgyroX_7, Lkoma1_77, LgyroY_7, Lkoma2_77, LgyroZ_7, Lpar2_77] = textread('Linus-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
+[Lpar1_88, LgyroX_8, Lkoma1_88, LgyroY_8, Lkoma2_88, LgyroZ_8, Lpar2_88] = textread('Linus-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
+[Lpar1_99, LgyroX_9, Lkoma1_99, LgyroY_9, Lkoma2_99, LgyroZ_9, Lpar2_99] = textread('Linus-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
+[Lpar1_100, LgyroX_10, Lkoma1_100, LgyroY_10, Lkoma2_100, LgyroZ_10, Lpar2_100] = textread('Linus-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
 
-[Epar1_11, EgyroX_1, Ekoma1_11, EgyroY_1, Ekoma2_11, EgyroZ_1, Epar2_11] = textread('Elin-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
-[Epar1_22, EgyroX_2, Ekoma1_22, EgyroY_2, Ekoma2_22, EgyroZ_2, Epar2_22] = textread('Elin-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
-[Epar1_33, EgyroX_3, Ekoma1_33, EgyroY_3, Ekoma2_33, EgyroZ_3, Epar2_33] = textread('Elin-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
-[Epar1_44, EgyroX_4, Ekoma1_44, EgyroY_4, Ekoma2_44, EgyroZ_4, Epar2_44] = textread('Elin-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
-[Epar1_55, EgyroX_5, Ekoma1_55, EgyroY_5, Ekoma2_55, EgyroZ_5, Epar2_55] = textread('Elin-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
-[Epar1_66, EgyroX_6, Ekoma1_66, EgyroY_6, Ekoma2_66, EgyroZ_6, Epar2_66] = textread('Elin-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
-[Epar1_77, EgyroX_7, Ekoma1_77, EgyroY_7, Ekoma2_77, EgyroZ_7, Epar2_77] = textread('Elin-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
-[Epar1_88, EgyroX_8, Ekoma1_88, EgyroY_8, Ekoma2_88, EgyroZ_8, Epar2_88] = textread('Elin-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
-[Epar1_99, EgyroX_9, Ekoma1_99, EgyroY_9, Ekoma2_99, EgyroZ_9, Epar2_99] = textread('Elin-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
-[Epar1_100, EgyroX_10, Ekoma1_100, EgyroY_10, Ekoma2_100, EgyroZ_10, Epar2_100] = textread('Elin-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
+[Mpar1_11, MgyroX_1, Mkoma1_11, MgyroY_1, Mkoma2_11, MgyroZ_1, Mpar2_11] = textread('Max-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
+[Mpar1_22, MgyroX_2, Mkoma1_22, MgyroY_2, Mkoma2_22, MgyroZ_2, Mpar2_22] = textread('Max-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
+[Mpar1_33, MgyroX_3, Mkoma1_33, MgyroY_3, Mkoma2_33, MgyroZ_3, Mpar2_33] = textread('Max-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
+[Mpar1_44, MgyroX_4, Mkoma1_44, MgyroY_4, Mkoma2_44, MgyroZ_4, Mpar2_44] = textread('Max-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
+[Mpar1_55, MgyroX_5, Mkoma1_55, MgyroY_5, Mkoma2_55, MgyroZ_5, Mpar2_55] = textread('Max-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
+[Mpar1_66, MgyroX_6, Mkoma1_66, MgyroY_6, Mkoma2_66, MgyroZ_6, Mpar2_66] = textread('Max-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
+[Mpar1_77, MgyroX_7, Mkoma1_77, MgyroY_7, Mkoma2_77, MgyroZ_7, Mpar2_77] = textread('Max-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
+[Mpar1_88, MgyroX_8, Mkoma1_88, MgyroY_8, Mkoma2_88, MgyroZ_8, Mpar2_88] = textread('Max-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
+[Mpar1_99, MgyroX_9, Mkoma1_99, MgyroY_9, Mkoma2_99, MgyroZ_9, Mpar2_99] = textread('Max-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
+[Mpar1_100, MgyroX_10, Mkoma1_100, MgyroY_10, Mkoma2_100, MgyroZ_10, Mpar2_100] = textread('Max-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
 
-[Epar1_11, EgyroX_1, Ekoma1_11, EgyroY_1, Ekoma2_11, EgyroZ_1, Epar2_11] = textread('Elin-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
-[Epar1_22, EgyroX_2, Ekoma1_22, EgyroY_2, Ekoma2_22, EgyroZ_2, Epar2_22] = textread('Elin-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
-[Epar1_33, EgyroX_3, Ekoma1_33, EgyroY_3, Ekoma2_33, EgyroZ_3, Epar2_33] = textread('Elin-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
-[Epar1_44, EgyroX_4, Ekoma1_44, EgyroY_4, Ekoma2_44, EgyroZ_4, Epar2_44] = textread('Elin-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
-[Epar1_55, EgyroX_5, Ekoma1_55, EgyroY_5, Ekoma2_55, EgyroZ_5, Epar2_55] = textread('Elin-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
-[Epar1_66, EgyroX_6, Ekoma1_66, EgyroY_6, Ekoma2_66, EgyroZ_6, Epar2_66] = textread('Elin-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
-[Epar1_77, EgyroX_7, Ekoma1_77, EgyroY_7, Ekoma2_77, EgyroZ_7, Epar2_77] = textread('Elin-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
-[Epar1_88, EgyroX_8, Ekoma1_88, EgyroY_8, Ekoma2_88, EgyroZ_8, Epar2_88] = textread('Elin-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
-[Epar1_99, EgyroX_9, Ekoma1_99, EgyroY_9, Ekoma2_99, EgyroZ_9, Epar2_99] = textread('Elin-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
-[Epar1_100, EgyroX_10, Ekoma1_100, EgyroY_10, Ekoma2_100, EgyroZ_10, Epar2_100] = textread('Elin-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
+[Rpar1_11, RgyroX_1, Rkoma1_11, RgyroY_1, Rkoma2_11, RgyroZ_1, Rpar2_11] = textread('Ruben-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
+[Rpar1_22, RgyroX_2, Rkoma1_22, RgyroY_2, Rkoma2_22, RgyroZ_2, Rpar2_22] = textread('Ruben-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
+[Rpar1_33, RgyroX_3, Rkoma1_33, RgyroY_3, Rkoma2_33, RgyroZ_3, Rpar2_33] = textread('Ruben-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
+[Rpar1_44, RgyroX_4, Rkoma1_44, RgyroY_4, Rkoma2_44, RgyroZ_4, Rpar2_44] = textread('Ruben-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
+[Rpar1_55, RgyroX_5, Rkoma1_55, RgyroY_5, Rkoma2_55, RgyroZ_5, Rpar2_55] = textread('Ruben-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
+[Rpar1_66, RgyroX_6, Rkoma1_66, RgyroY_6, Rkoma2_66, RgyroZ_6, Rpar2_66] = textread('Ruben-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
+[Rpar1_77, RgyroX_7, Rkoma1_77, RgyroY_7, Rkoma2_77, RgyroZ_7, Rpar2_77] = textread('Ruben-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
+[Rpar1_88, RgyroX_8, Rkoma1_88, RgyroY_8, Rkoma2_88, RgyroZ_8, Rpar2_88] = textread('Ruben-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
+[Rpar1_99, RgyroX_9, Rkoma1_99, RgyroY_9, Rkoma2_99, RgyroZ_9, Rpar2_99] = textread('Ruben-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
+[Rpar1_100, RgyroX_10, Rkoma1_100, RgyroY_10, Rkoma2_100, RgyroZ_10, Rpar2_100] = textread('Ruben-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
 
-[Epar1_11, EgyroX_1, Ekoma1_11, EgyroY_1, Ekoma2_11, EgyroZ_1, Epar2_11] = textread('Elin-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
-[Epar1_22, EgyroX_2, Ekoma1_22, EgyroY_2, Ekoma2_22, EgyroZ_2, Epar2_22] = textread('Elin-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
-[Epar1_33, EgyroX_3, Ekoma1_33, EgyroY_3, Ekoma2_33, EgyroZ_3, Epar2_33] = textread('Elin-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
-[Epar1_44, EgyroX_4, Ekoma1_44, EgyroY_4, Ekoma2_44, EgyroZ_4, Epar2_44] = textread('Elin-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
-[Epar1_55, EgyroX_5, Ekoma1_55, EgyroY_5, Ekoma2_55, EgyroZ_5, Epar2_55] = textread('Elin-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
-[Epar1_66, EgyroX_6, Ekoma1_66, EgyroY_6, Ekoma2_66, EgyroZ_6, Epar2_66] = textread('Elin-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
-[Epar1_77, EgyroX_7, Ekoma1_77, EgyroY_7, Ekoma2_77, EgyroZ_7, Epar2_77] = textread('Elin-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
-[Epar1_88, EgyroX_8, Ekoma1_88, EgyroY_8, Ekoma2_88, EgyroZ_8, Epar2_88] = textread('Elin-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
-[Epar1_99, EgyroX_9, Ekoma1_99, EgyroY_9, Ekoma2_99, EgyroZ_9, Epar2_99] = textread('Elin-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
-[Epar1_100, EgyroX_10, Ekoma1_100, EgyroY_10, Ekoma2_100, EgyroZ_10, Epar2_100] = textread('Elin-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
-
-
+[Jpar1_11, JgyroX_1, Jkoma1_11, JgyroY_1, Jkoma2_11, JgyroZ_1, Jpar2_11] = textread('Julius-20meanGyroList-1.txt', '%c %f %c %f %c %f %c');
+[Jpar1_22, JgyroX_2, Jkoma1_22, JgyroY_2, Jkoma2_22, JgyroZ_2, Jpar2_22] = textread('Julius-20meanGyroList-2.txt', '%c %f %c %f %c %f %c');
+[Jpar1_33, JgyroX_3, Jkoma1_33, JgyroY_3, Jkoma2_33, JgyroZ_3, Jpar2_33] = textread('Julius-20meanGyroList-3.txt', '%c %f %c %f %c %f %c');
+[Jpar1_44, JgyroX_4, Jkoma1_44, JgyroY_4, Jkoma2_44, JgyroZ_4, Jpar2_44] = textread('Julius-20meanGyroList-4.txt', '%c %f %c %f %c %f %c');
+[Jpar1_55, JgyroX_5, Jkoma1_55, JgyroY_5, Jkoma2_55, JgyroZ_5, Jpar2_55] = textread('Julius-20meanGyroList-5.txt', '%c %f %c %f %c %f %c');
+[Jpar1_66, JgyroX_6, Jkoma1_66, JgyroY_6, Jkoma2_66, JgyroZ_6, Jpar2_66] = textread('Julius-20meanGyroList-6.txt', '%c %f %c %f %c %f %c');
+[Jpar1_77, JgyroX_7, Jkoma1_77, JgyroY_7, Jkoma2_77, JgyroZ_7, Jpar2_77] = textread('Julius-20meanGyroList-7.txt', '%c %f %c %f %c %f %c');
+[Jpar1_88, JgyroX_8, Jkoma1_88, JgyroY_8, Jkoma2_88, JgyroZ_8, Jpar2_88] = textread('Julius-20meanGyroList-8.txt', '%c %f %c %f %c %f %c');
+[Jpar1_99, JgyroX_9, Jkoma1_99, JgyroY_9, Jkoma2_99, JgyroZ_9, Jpar2_99] = textread('Julius-20meanGyroList-9.txt', '%c %f %c %f %c %f %c');
+[Jpar1_100, JgyroX_10, Jkoma1_100, JgyroY_10, Jkoma2_100, JgyroZ_10, Jpar2_100] = textread('Julius-20meanGyroList-10.txt', '%c %f %c %f %c %f %c');
 
 
 %% Plotta ut för varje person
