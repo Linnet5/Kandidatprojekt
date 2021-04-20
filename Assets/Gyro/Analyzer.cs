412b2b330d79d34217ff2ee2078445714ad4964b
Julius Halldan <juliushalldan@gmail.com>
2021-04-20 10:41:53 +0200

    With analyzer

---
+++ Assets/Gyro/Analyzer.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Globalization;
+
+
+public class Analyzer : MonoBehaviour
+{
+
+    private bool result;
+    private int standardNom = 20; 
+    private int numberOfMeans;
+    int samplesPerMean;
+    float samplesPerMeanF;
+
+    List<Vector3> accelBuffer;
+    List<Vector3> gyroBuffer;
+
+    private List<Vector3> referenceAccel;
+    private List<Vector3> referenceGyro;
+    private List<Vector3> meanReferenceAccel;
+    private List<Vector3> meanReferenceGyro;
+    private List<Vector3> meanAccel;
+    private List<Vector3> meanGyro;
+    Vector3 inputSum;
+    private float floatConversion;
+    private GameObject NomInputField;
+    bool movement = false;
+    bool record = false;
+
+
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
+    private void Start()
+    {
+        NomInputField = GameObject.Find("NomInputField");
+        numberOfMeans = standardNom;
+
+        accelBuffer = new List<Vector3>();
+        gyroBuffer = new List<Vector3>();
+
+        for (int i = 0; i < 130; i++) {
+            accelBuffer.Add(Vector3.zero);
+            gyroBuffer.Add(Vector3.zero);
+        }
+
+        meanReferenceAccel = new List<Vector3>();
+        meanReferenceGyro = new List<Vector3>();
+
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        //Read TextAssets with two loops
+        //string filePath = "Assets/Resources/Reference Moves/Squat/";
+        TextAsset gyro = Resources.Load("Reference Moves/Squat/meanReferenceGyro") as TextAsset;
+        TextAsset accel = Resources.Load("Reference Moves/Squat/meanReferenceAccel") as TextAsset;
+
+        string[] lines = gyro.text.Split('\n');
+        string[] vectorString;
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+
+            vectorString = lines[i].Split(' ');
+
+            meanReferenceGyro.Add(new Vector3(float.Parse(vectorString[0], CultureInfo.InvariantCulture), float.Parse(vectorString[1], CultureInfo.InvariantCulture), float.Parse(vectorString[2], CultureInfo.InvariantCulture)));
+        }
+
+
+    }
+
+    private void Update()
+    {
+        accelBuffer.RemoveAt(129);
+        accelBuffer.Insert(0, Input.gyro.userAcceleration);
+
+        gyroBuffer.RemoveAt(129);
+        gyroBuffer.Insert(0, Input.gyro.gravity);
+    }
+
+    private List<Vector3> CreateMeans(List<Vector3> input)
+    {
+        if (!(NomInputField.GetComponent<InputField>().text == ""))
+        {
+            numberOfMeans = int.Parse(NomInputField.GetComponent<InputField>().text);
+        }
+
+        else {
+            numberOfMeans = standardNom;
+        }
+
+        //Debug.Log(numberOfMeans);
+
+        
+        List<Vector3> output = new List<Vector3>();
+
+        
+        samplesPerMean = Mathf.CeilToInt(input.Count / numberOfMeans);
+        //samplesPerMeanF = input.Count / numberOfMeans;
+        //samplesPerMean = input.Count / (float)numberOfMeans;
+        int totalSamples = samplesPerMean * numberOfMeans;
+        int kompIterations = totalSamples - input.Count;
+
+        for (int i = 0; i < kompIterations; i++)
+        {
+            input.Add(Vector3.zero);
+        }
+
+
+        for (int i = 0; i < numberOfMeans; i++)
+        {
+            inputSum = Vector3.zero;
+            for (int j = i * samplesPerMean; j < (i + 1) * samplesPerMean; j++)
+            {
+                inputSum = inputSum + input[j];
+            }
+            floatConversion = (float)samplesPerMean;
+            output.Add(inputSum / floatConversion);
+            //Debug.Log((inputSum/floatConversion).ToString("F9"));
+        }
+
+
+
+        return output;
+    }
+
+    
+    private float Analyze(List<Vector3> accel, List<Vector3> gyro) {
+
+        meanAccel = CreateMeans(accel);
+        meanGyro = CreateMeans(gyro);
+
+        Vector3 deltaAccel = Vector3.zero;
+        Vector3 deltaGyro = Vector3.zero;
+
+        for (int i = 0; i < meanReferenceAccel.Count; i++)
+        {
+            deltaAccel += (meanReferenceAccel[i] - meanAccel[i]);
+        }
+        deltaAccel /= meanReferenceAccel.Count;
+
+        for (int i = 0; i < meanReferenceGyro.Count; i++) {
+            //deltaGyro += (meanReferenceGyro[i] - new Vector3(Mathf.Abs(meanAccel[i].x), Mathf.Abs(meanAccel[i].y), Mathf.Abs(meanAccel[i].z)));
+            deltaGyro.x += Mathf.Abs(meanReferenceGyro[i].x - Mathf.Abs(meanGyro[i].x));
+            deltaGyro.y += Mathf.Abs(meanReferenceGyro[i].y - Mathf.Abs(meanGyro[i].y));
+            deltaGyro.z += Mathf.Abs(meanReferenceGyro[i].z - Mathf.Abs(meanGyro[i].z));
+        }
+        deltaGyro /= (float)(meanReferenceGyro.Count);
+
+        return deltaGyro.y;
+
+
+    }
+
+    public List<Vector3> GetMeans(List<Vector3> input) {
+        
+        return CreateMeans(input);
+
+    }
+
+    public int GetNom()
+    {
+        return numberOfMeans;
+    }
+
+    public float GetResult() {
+
+        float result = 0;
+
+        result = 1 - Analyze(accelBuffer,gyroBuffer);
+        result -= 0.2f;
+
+        result *= 1.124f;
+
+
+        accelBuffer = new List<Vector3>();
+        gyroBuffer = new List<Vector3>();
+
+        for (int i = 0; i < 130; i++)
+        {
+            accelBuffer.Add(Vector3.zero);
+            gyroBuffer.Add(Vector3.zero);
+        }
+
+        return result;
+    }
+
+}
