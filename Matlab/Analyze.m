1069a6f14f3292f6f3e1f4c4bc59a9453f784e35
Julius Halldan <juliushalldan@gmail.com>
2021-04-29 13:18:58 +0200

    matlab

--- Matlab/Analyze.m
+++ Matlab/Analyze.m
@@ -120,12 +120,12 @@ mnTOT(:,3) = mnZ;
 %FID = fopen('meanReferenceGyro.txt', 'w');
 %fprintf(FID,'%0.4f %1.4f %1.4f\n',mnTOT);
 
-delta = sum(abs(mnY-abs(accelY_33)))/50;
+delta = sum(abs(mnX-abs(accelX_33)))/50;
 %delta = 0.09;
 hold on;
 
-scatter(t,abs(accelY_33)); 
-plot(t,mnY); ylim([0,1]);
+scatter(t,abs(accelX_33)); 
+plot(t,mnX); ylim([-1,1]);
 %plot(t,mnY-delta);
 %plot(t,mnY+delta);
 
@@ -220,8 +220,8 @@ plot(t,mnY); ylim([0,1]);
 clear;
 clc;
 
-nom = '20';
-gyroOrAccel = 'meanAccel';
+nom = '50';
+gyroOrAccel = 'meanGyro';
 
 %Append bygger ihop strukturen på namnet, så att man kan ändra det smidigt
 %midFileName = append('Elin-',nom,gyroOrAccel,'List-1.txt');
@@ -271,26 +271,54 @@ figure;
 [par1_39, accelX_39, koma1_39, accelY_39, koma2_39, accelZ_39, par2_39] = textread(append('Elin-',nom,gyroOrAccel,'List-9.txt'), '%c %f %c %f %c %f %c');
 [par1_40, accelX_40, koma1_40, accelY_40, koma2_40, accelZ_40, par2_40] = textread(append('Elin-',nom,gyroOrAccel,'List-10.txt'), '%c %f %c %f %c %f %c');
 
+[par1_41, accelX_41, koma1_41, accelY_41, koma2_41, accelZ_41, par2_41] = textread(append('Ruben-',nom,gyroOrAccel,'List-1.txt'), '%c %f %c %f %c %f %c');
+[par1_42, accelX_42, koma1_42, accelY_42, koma2_42, accelZ_42, par2_42] = textread(append('Ruben-',nom,gyroOrAccel,'List-2.txt'), '%c %f %c %f %c %f %c');
+[par1_43, accelX_43, koma1_43, accelY_43, koma2_43, accelZ_43, par2_43] = textread(append('Ruben-',nom,gyroOrAccel,'List-3.txt'), '%c %f %c %f %c %f %c');
+[par1_44, accelX_44, koma1_44, accelY_44, koma2_44, accelZ_44, par2_44] = textread(append('Ruben-',nom,gyroOrAccel,'List-4.txt'), '%c %f %c %f %c %f %c');
+[par1_45, accelX_45, koma1_45, accelY_45, koma2_45, accelZ_45, par2_45] = textread(append('Ruben-',nom,gyroOrAccel,'List-5.txt'), '%c %f %c %f %c %f %c');
+[par1_46, accelX_46, koma1_46, accelY_46, koma2_46, accelZ_46, par2_46] = textread(append('Ruben-',nom,gyroOrAccel,'List-6.txt'), '%c %f %c %f %c %f %c');
+[par1_47, accelX_47, koma1_47, accelY_47, koma2_47, accelZ_47, par2_47] = textread(append('Ruben-',nom,gyroOrAccel,'List-7.txt'), '%c %f %c %f %c %f %c');
+[par1_48, accelX_48, koma1_48, accelY_48, koma2_48, accelZ_48, par2_48] = textread(append('Ruben-',nom,gyroOrAccel,'List-8.txt'), '%c %f %c %f %c %f %c');
+[par1_49, accelX_49, koma1_49, accelY_49, koma2_49, accelZ_49, par2_49] = textread(append('Ruben-',nom,gyroOrAccel,'List-9.txt'), '%c %f %c %f %c %f %c');
+[par1_50, accelX_50, koma1_50, accelY_50, koma2_50, accelZ_50, par2_50] = textread(append('Ruben-',nom,gyroOrAccel,'List-10.txt'), '%c %f %c %f %c %f %c');
+
 t = 1:1:length(accelX_4);
 
 %mn = (abs(accelX_1) + abs(accelX_2) + abs(accelX_3) + abs(accelX_4) + abs(accelX_5) + abs(accelX_6) + abs(accelX_7) + abs(accelX_8) + abs(accelX_9) + abs(accelX_10) + abs(accelX_11) + abs(accelX_12) + abs(accelX_13) + abs(accelX_14) + abs(accelX_15) + abs(accelX_16) + abs(accelX_17) + abs(accelX_18) + abs(accelX_19) + abs(accelX_20) + abs(accelX_21) + abs(accelX_22) + abs(accelX_23) + abs(accelX_24) + abs(accelX_25) + abs(accelX_26) + abs(accelX_27) + abs(accelX_28) + abs(accelX_29) + abs(accelX_30) + abs(accelX_31) + abs(accelX_32) + abs(accelX_33) + abs(accelX_34) + abs(accelX_35) + abs(accelX_36) + abs(accelX_37) + abs(accelX_38) + abs(accelX_39) + abs(accelX_40))/40;
 
-mnX = (accelX_1 + accelX_2 + accelX_3 + accelX_4 + accelX_5 + accelX_6 + accelX_7 + accelX_8 + accelX_9 + accelX_10 + accelX_11 + accelX_12 + accelX_13 + accelX_14 + accelX_15 + accelX_16 + accelX_17 + accelX_18 + accelX_19 + accelX_20 + accelX_21 + accelX_22 + accelX_23 + accelX_24 + accelX_25 + accelX_26 + accelX_27 + accelX_28 + accelX_29 + accelX_30 + accelX_31 + accelX_32 + accelX_33 + accelX_34 + accelX_35 + accelX_36 + accelX_37 + accelX_38 + accelX_39 + accelX_40)/40;
-mnY = (accelY_1 + accelY_2 + accelY_3 + accelY_4 + accelY_5 + accelY_6 + accelY_7 + accelY_8 + accelY_9 + accelY_10 + accelY_11 + accelY_12 + accelY_13 + accelY_14 + accelY_15 + accelY_16 + accelY_17 + accelY_18 + accelY_19 + accelY_20 + accelY_21 + accelY_22 + accelY_23 + accelY_24 + accelY_25 + accelY_26 + accelY_27 + accelY_28 + accelY_29 + accelY_30 + accelY_31 + accelY_32 + accelY_33 + accelY_34 + accelY_35 + accelY_36 + accelY_37 + accelY_38 + accelY_39 + accelY_40)/40;
-mnZ = (accelZ_1 + accelZ_2 + accelZ_3 + accelZ_4 + accelZ_5 + accelZ_6 + accelZ_7 + accelZ_8 + accelZ_9 + accelZ_10 + accelZ_11 + accelZ_12 + accelZ_13 + accelZ_14 + accelZ_15 + accelZ_16 + accelZ_17 + accelZ_18 + accelZ_19 + accelZ_20 + accelZ_21 + accelZ_22 + accelZ_23 + accelZ_24 + accelZ_25 + accelZ_26 + accelZ_27 + accelZ_28 + accelZ_29 + accelZ_30 + accelZ_31 + accelZ_32 + accelZ_33 + accelZ_34 + accelZ_35 + accelZ_36 + accelZ_37 + accelZ_38 + accelZ_39 + accelZ_40)/40;
+mnX = (accelX_1 + accelX_2 + accelX_3 + accelX_4 + accelX_5 + accelX_6 + accelX_7 + accelX_8 + accelX_9 + accelX_10 + accelX_11 + accelX_12 + accelX_13 + accelX_14 + accelX_15 + accelX_16 + accelX_17 + accelX_18 + accelX_19 + accelX_20 + accelX_21 + accelX_22 + accelX_23 + accelX_24 + accelX_25 + accelX_26 + accelX_27 + accelX_28 + accelX_29 + accelX_30 + accelX_31 + accelX_32 + accelX_33 + accelX_34 + accelX_35 + accelX_36 + accelX_37 + accelX_38 + accelX_39 + accelX_40 + accelX_41 + accelX_42 + accelX_43 + accelX_44 + accelX_45 + accelX_46 + accelX_47 + accelX_48 + accelX_49 + accelX_50)/50;
+mnY = (accelY_1 + accelY_2 + accelY_3 + accelY_4 + accelY_5 + accelY_6 + accelY_7 + accelY_8 + accelY_9 + accelY_10 + accelY_11 + accelY_12 + accelY_13 + accelY_14 + accelY_15 + accelY_16 + accelY_17 + accelY_18 + accelY_19 + accelY_20 + accelY_21 + accelY_22 + accelY_23 + accelY_24 + accelY_25 + accelY_26 + accelY_27 + accelY_28 + accelY_29 + accelY_30 + accelY_31 + accelY_32 + accelY_33 + accelY_34 + accelY_35 + accelY_36 + accelY_37 + accelY_38 + accelY_39 + accelY_40 + accelY_41 + accelY_42 + accelY_43 + accelY_44 + accelY_45 + accelY_46 + accelY_47 + accelY_48 + accelY_49 + accelY_50)/50;
+mnZ = (accelZ_1 + accelZ_2 + accelZ_3 + accelZ_4 + accelZ_5 + accelZ_6 + accelZ_7 + accelZ_8 + accelZ_9 + accelZ_10 + accelZ_11 + accelZ_12 + accelZ_13 + accelZ_14 + accelZ_15 + accelZ_16 + accelZ_17 + accelZ_18 + accelZ_19 + accelZ_20 + accelZ_21 + accelZ_22 + accelZ_23 + accelZ_24 + accelZ_25 + accelZ_26 + accelZ_27 + accelZ_28 + accelZ_29 + accelZ_30 + accelZ_31 + accelZ_32 + accelZ_33 + accelZ_34 + accelZ_35 + accelZ_36 + accelZ_37 + accelZ_38 + accelZ_39 + accelZ_40 + accelZ_41 + accelZ_42 + accelZ_43 + accelZ_44 + accelZ_45 + accelZ_46 + accelZ_47 + accelZ_48 + accelZ_49 + accelZ_50)/50;
+
+
+
+
+
+[par1_51, accelX_51, koma1_51, accelY_51, koma2_51, accelZ_51, par2_51] = textread(append('Elinfel-50meanGyroList-12.txt'), '%c %f %c %f %c %f %c');
+
+%delta = 0.35;
+hold on;
+theRecMax = max(abs(min(accelY_50)),max(accelY_50));
+accelY_50 = accelY_50 * (1/theRecMax);
+
+theRefXMax = max(abs(min(mnX)),max(mnX));
+mnX = mnX * (1/theRefXMax);
+
+theRefYMax = max(abs(min(mnY)),max(mnY));
+mnY = mnY * (1/theRefYMax);
+
+theRefZMax = max(abs(min(mnZ)),max(mnZ));
+mnZ = mnZ * (1/theRefZMax);
+
+delta = sum(abs(mnY-accelY_50))/50;
+scatter(t,accelY_50); ylim([-1,1]);
+plot(t,mnZ);
+%plot(t,mnX-delta);
+%plot(t,mnX+delta);
 
 mnTOT(:,1) = mnX;
 mnTOT(:,2) = mnY;
 mnTOT(:,3) = mnZ;
 
-%FID = fopen('meanReferenceAccel.txt', 'w');
+%FID = fopen('meanReferenceGyro.txt', 'w');
 %fprintf(FID,'%0.4f %1.4f %1.4f\n',mnTOT);
-
-delta = sum(abs(mnY-accelY_3))/20;
-%delta = 0.09;
-hold on;
-
-scatter(t,accelY_3); 
-plot(t,mnY);
-%plot(t,mnX-delta);
-%plot(t,mnX+delta);
