cb8866fb229dd731d20e3d02e47dc46c366bbb05
Julius Halldan <juliushalldan@gmail.com>
2021-04-30 16:36:47 +0200

    With output

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -134,7 +134,7 @@ public class Goal : MonoBehaviour
         float accuracy = GameObject.Find("Analyzer").GetComponent<Analyzer>().GetResult();
         Debug.Log(accuracy);
 
-        //GameObject.Find("Calibration").GetComponent<ParentCoordinates>().bodyText.GetComponent<TMPro.TextMeshProUGUI>().SetText("dE: " + accuracy);
+        GameObject.Find("Calibration").GetComponent<ParentCoordinates>().bodyText.GetComponent<TMPro.TextMeshProUGUI>().SetText("dE: " + accuracy);
 
         //@elin kan behöva ändra accuracy
         if (accuracy > 0.85f)
